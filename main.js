b32be882792b680748a2a745ccc140b211947f9e
saurabhraj80923-dotcom <saurabhraj80923@gmail.com>
2026-06-06 12:07:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -91,42 +91,18 @@ if (multipleText) {
   rotateWords();
 }
 
-/* ================= MOBILE MENU ================= */
+
+/* ===== MOBILE MENU ===== */
 
 const menuIcon = document.querySelector('#menu-icon');
 const navbar = document.querySelector('.navbar');
 
 if(menuIcon){
-    menuIcon.onclick = () => {
-        navbar.classList.toggle('active');
 
-        if(menuIcon.classList.contains('bx-menu')){
-            menuIcon.classList.replace('bx-menu','bx-x');
-        }else{
-            menuIcon.classList.replace('bx-x','bx-menu');
-        }
-    }
+menuIcon.onclick = () => {
+
+    navbar.classList.toggle('active');
+
+};
+
 }
-
-/* CLOSE MENU ON CLICK */
-
-document.querySelectorAll('.navbar a').forEach(link => {
-    link.onclick = () => {
-        navbar.classList.remove('active');
-        menuIcon.classList.replace('bx-x','bx-menu');
-    }
-});
-
-/* HEADER SHADOW ON SCROLL */
-
-window.addEventListener('scroll', () => {
-
-    const header = document.querySelector('.header');
-
-    if(window.scrollY > 50){
-        header.style.boxShadow =
-        "0 0 25px rgba(47,201,255,0.15)";
-    }else{
-        header.style.boxShadow = "none";
-    }
-});
