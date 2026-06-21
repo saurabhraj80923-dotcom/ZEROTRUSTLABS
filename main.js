1e125e87d97d24631930179059e92b9cbdcd8447
saurabhraj80923-dotcom <saurabhraj80923@gmail.com>
2026-06-21 10:00:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -127,3 +127,16 @@ document.querySelectorAll('.navbar a').forEach(link => {
     });
 
 });
+
+const readMoreBtn = document.getElementById("readMoreBtn");
+const moreContent = document.getElementById("more-content");
+
+readMoreBtn.addEventListener("click", () => {
+    if (moreContent.style.display === "none") {
+        moreContent.style.display = "block";
+        readMoreBtn.textContent = "Read Less";
+    } else {
+        moreContent.style.display = "none";
+        readMoreBtn.textContent = "Read More";
+    }
+});
